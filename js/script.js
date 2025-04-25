454c87f36a42842e57c699129a8a20bd086db58b
Denys Bludov <denys.bludov@stu.ocsb.ca>
2025-04-25 14:36:25 +0000

    finished

--- js/script.js
+++ js/script.js
@@ -3,3 +3,11 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+function calcVolume() {
+    let radius = parseFloat(document.getElementById("radius").value)
+    let step1 = 4 / 3
+    let step2 = radius * radius * radius
+    let step3 = step1 * (Math.PI * step2)
+    let answer = step3.toFixed(2)
+    document.getElementById("answer").innerHTML = "V ≈" + answer +"cm";
+}
